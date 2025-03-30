90c3b25ad08d16c7ff9389d6863799a371d5f282
thibault-cuperlier <thibcup@gmail.com>
2025-03-30 11:50:26 +0200

    t

--- src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://mcjucbmvurmebbjqmqjc.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1janVjYm12dXJtZWJianFtcWpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMyNjg5ODQsImV4cCI6MjA1ODg0NDk4NH0.xPiHDlUj89h3mrTNNb9rVup0vdX1JqvpEjsLv42jKRE";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
